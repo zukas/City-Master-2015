de8e84468fcfa3ba63d7daf32a184831d528f40e
zukas <julius.zukauskas@gmx.com>
2015-10-01 19:12:44 +0200

    added fine name to validator

--- Util/glvalidator.cpp
+++ Util/glvalidator.cpp
@@ -11,29 +11,29 @@ void check_gl_error(const char *file, unsigned int line, const char *function) {
 	case GL_NO_ERROR:
 		break;
 	case GL_INVALID_ENUM:
-		printf("OpenGL encountered a GL_INVALID_ENUM error: %s(%u)\n", function,
-			   line);
+		printf("OpenGL encountered a GL_INVALID_ENUM error: %s %s(%u)\n", file,
+			   function, line);
 		break;
 	case GL_INVALID_VALUE:
-		printf("OpenGL encountered a GL_INVALID_VALUE error: %s(%u)\n",
+		printf("OpenGL encountered a GL_INVALID_VALUE error: %s %s(%u)\n", file,
 			   function, line);
 		break;
 	case GL_INVALID_OPERATION:
-		printf("OpenGL encountered a GL_INVALID_OPERATION error: %s(%u)\n",
-			   function, line);
+		printf("OpenGL encountered a GL_INVALID_OPERATION error: %s %s(%u)\n",
+			   file, function, line);
 		break;
 	case GL_INVALID_FRAMEBUFFER_OPERATION:
 		printf("OpenGL encountered a GL_INVALID_FRAMEBUFFER_OPERATION error: "
-			   "%s(%u)\n",
-			   function, line);
+			   "%s %s(%u)\n",
+			   file, function, line);
 		break;
 	case GL_OUT_OF_MEMORY:
-		printf("OpenGL encountered a GL_OUT_OF_MEMORY error: %s(%u)\n",
+		printf("OpenGL encountered a GL_OUT_OF_MEMORY error: %s %s(%u)\n", file,
 			   function, line);
 		break;
 	default:
-		printf("OpenGL encountered a %d error: %s(%u)\n", error, function,
-			   line);
+		printf("OpenGL encountered a %d error: %s %s(%u)\n", error, file,
+			   function, line);
 		break;
 	}
 }
@@ -45,49 +45,49 @@ void check_gl_framebuffer_error(const char *file, unsigned int line,
 	case GL_FRAMEBUFFER_COMPLETE:
 		break;
 	case GL_FRAMEBUFFER_UNDEFINED:
-		printf("OpenGL encountered a GL_FRAMEBUFFER_UNDEFINED error: %s(%u)\n",
-			   function, line);
+		printf("OpenGL encountered a GL_FRAMEBUFFER_UNDEFINED error: %s %s(%u)\n",
+			   file, function, line);
 		break;
 	case GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
 		printf("OpenGL encountered a GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT "
-			   "error: %s(%u)\n",
-			   function, line);
+			   "error: %s %s(%u)\n",
+			   file, function, line);
 		break;
 	case GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
 		printf("OpenGL encountered a "
-			   "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT error: %s(%u)\n",
-			   function, line);
+			   "GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT error: %s %s(%u)\n",
+			   file, function, line);
 		break;
 	case GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
 		printf(
 			"OpenGL encountered a GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER error: "
-			"%s(%u)\n",
-			function, line);
+			"%s %s(%u)\n",
+			file, function, line);
 		break;
 	case GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
 		printf(
 			"OpenGL encountered a GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER error: "
-			"%s(%u)\n",
-			function, line);
+			"%s %s(%u)\n",
+			file, function, line);
 		break;
 	case GL_FRAMEBUFFER_UNSUPPORTED:
 		printf("OpenGL encountered a GL_FRAMEBUFFER_UNSUPPORTED error: "
-			   "%s(%u)\n",
-			   function, line);
+			   "%s %s(%u)\n",
+			   file, function, line);
 		break;
 	case GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
 		printf(
 			"OpenGL encountered a GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE error: "
-			"%s(%u)\n",
-			function, line);
+			"%s %s(%u)\n",
+			file, function, line);
 		break;
 	case GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
 		printf("OpenGL encountered a GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS "
-			   "error: %s(%u)\n",
-			   function, line);
+			   "error: %s %s(%u)\n",
+			   file, function, line);
 		break;
 	default:
-		printf("OpenGL encountered a %d error: %s(%u)\n", error, function,
+		printf("OpenGL encountered a %d error: %s %s(%u)\n", error, file, function,
 			   line);
 		break;
 	}
